33801d4f5743464118005c30574fe9b0c005ceb8
MohFahmi27 <mohammadfahmi417@gmail.com>
2020-08-13 21:38:06 +0800

    add product + fix image in navbar.

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,38 @@
 $(".slider-nav").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    asNavFor: ".slider-for",
     dots: false,
-    focusOnSelect: true
+    autoplay: true,
+    autoplaySpeed: 2000,
+    focusOnSelect: true,
   });
+
+$(".slider-product").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    focusOnSelect: true,
+    responsive: [
+        {
+          breakpoint: 1025,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 426,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    ]
+});  
